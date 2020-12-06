92134e78e663558cda4c7e49d3b58a977d97e0de
Tim de Klijn <tim.de.klijn@gmail.com>
2020-12-06 10:37:10 +0100

    Cleaned up code

--- day06/day_6.go
+++ day06/day_6.go
@@ -8,17 +8,25 @@ import (
 )
 
 func countCommonAnswers(s *bufio.Scanner) int {
-	total := 0
-	groupSize := 0
-	group := make(map[byte]int)
+	total := 0                  // total unique yes answers per group
+	groupSize := 0              // Counter to check if the whole group answered yes
+	group := make(map[byte]int) // group map
+
 	for s.Scan() {
+
 		row := strings.Trim(s.Text(), "\n")
+
 		if len(row) == 0 {
+
+			// If the number of 'yes' answers is equal to group size, we add it to the
+			// total
 			for _, v := range group {
 				if v == groupSize {
 					total++
 				}
 			}
+
+			// Reset the counters
 			group = make(map[byte]int)
 			groupSize = 0
 		} else {
@@ -29,24 +37,31 @@ func countCommonAnswers(s *bufio.Scanner) int {
 		}
 	}
 
-	// TODO: HOW COME I HAVE TO DO THIS?
+	// UGLY: also do last group
 	for _, v := range group {
 		if v == groupSize {
 			total++
 		}
 	}
+
+	// Handle io error
 	if err := s.Err(); err != nil {
 		panic(err)
 	}
+
 	return total
 }
 
 func countUniqueAnswers(s *bufio.Scanner) int {
-	total := 0
-	group := make(map[byte]int)
+	total := 0                  // Count the number of 'yes'es
+	group := make(map[byte]int) // Per answer count the number of yes
+
 	for s.Scan() {
+
 		row := strings.Trim(s.Text(), "\n")
+
 		if len(row) == 0 {
+			// If group is checked. Add the number of yes answers to the total
 			total += len(group)
 			group = make(map[byte]int)
 		} else {
@@ -55,10 +70,15 @@ func countUniqueAnswers(s *bufio.Scanner) int {
 			}
 		}
 	}
+
+	// UGLY: also do last group
 	total += len(group)
+
+	// Handle io error
 	if err := s.Err(); err != nil {
 		panic(err)
 	}
+
 	return total
 }
 
